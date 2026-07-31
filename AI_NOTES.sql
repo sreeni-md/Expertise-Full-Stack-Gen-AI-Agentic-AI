28c61d1f87c72ab7758501fe53ee80bfc6ccbaa2
Sreeni Marripudi <sreeni.md01@gmail.com>
2026-07-31 16:24:39 -0400

    Add files via upload
    AI course

---
+++ AI_NOTES.sql
@@ -0,0 +1,6 @@
+-----GENERATIVE AI/AGENTIC AI-----
+--1.GITHUB.COM 
+1.login with gmail, sreeni.md01
+2.provide the username sreeni-md 
+3.set up is ready.
+4.go to the profile fill it need ful things.
